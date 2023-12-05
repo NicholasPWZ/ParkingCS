ad2769b85c89906557b5ece9c38c3b8ad0114531
Nicholas <nicholcosta@hotmail.com>
2023-12-05 09:00:37 -0300

    First final version

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ParkingLotSys.models;
+
+Console.WriteLine("Informe o preço base do estacionamento:");
+string? precoBasestr = Console.ReadLine();
+Console.WriteLine("Informe o preço por HORA do estacionamento:");
+string? precoHorastr = Console.ReadLine();
+double precoBase = Double.Parse(precoBasestr);
+double precoHora = Double.Parse(precoHorastr);
+
+Estacionamento estacionamento = new Estacionamento(precoBase, precoHora);
+estacionamento.carros = new List<string>();
+while (true){
+    Console.WriteLine("Selecione a sua próxima ação:\n1 - Adicionar carro\n2 - Remover carro\n3 - Listar carros\n4 - Encerrar programa");
+    string? acao = Console.ReadLine();
+    if (acao == "1")
+    {
+        Console.WriteLine("Informe a placa do veículo:\n");
+        string? placa = Console.ReadLine();
+        estacionamento.AdicionarCarro(placa);
+    }
+    else if (acao == "2")
+    {
+        Console.WriteLine("Informe a placa do veículo:\n");
+        string? placa = Console.ReadLine();
+        estacionamento.RemoverCarro(placa);
+    }
+    else if (acao == "3")
+    {
+        estacionamento.ListarCarros();
+    }
+    else if (acao == "4")
+    {
+        Console.WriteLine("Finalizando o programa :)");
+        break;
+    }
+}
+
